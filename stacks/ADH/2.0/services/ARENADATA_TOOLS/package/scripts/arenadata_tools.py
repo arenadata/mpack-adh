c87e907d705352abc8fc71359c5a14c8c9839c59
Alexander Ermakov <ean@arenadata.io>
2018-10-15 10:28:11 +0300

    ADH-706 Added Symlink for Short Circuit Read

--- stacks/ADH/2.0/services/ARENADATA_TOOLS/package/scripts/arenadata_tools.py
+++ stacks/ADH/2.0/services/ARENADATA_TOOLS/package/scripts/arenadata_tools.py
@@ -1,9 +1,11 @@
+from resource_management.core.resources.system import Execute
 from resource_management import *
 
 class arenadata_tools(Script):
     def install(self, env):
         Logger.info("Installing Arenadata Tools packages")
         self.install_packages(env)
+        Execute ('ln -s /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64/jre/lib/amd64/server/libjvm.so /usr/lib64/libjvm.so')
         #if any other install steps were needed they can be added here
 
     def configure(self,env):
