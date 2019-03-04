0cfaf800e0fd807c00c1738ad4bde9d6c949d381
Alexander Ermakov <ean@arenadata.io>
2019-03-04 17:52:16 +0300

    ADH-706 Fixed Short Circuit Read Symlink Creation

--- stacks/ADH/2.0/services/ARENADATA_TOOLS/package/scripts/arenadata_tools.py
+++ stacks/ADH/2.0/services/ARENADATA_TOOLS/package/scripts/arenadata_tools.py
@@ -1,10 +1,12 @@
 from resource_management import *
+from resource_management.core.resources.system import Execute
 
 class arenadata_tools(Script):
     def install(self, env):
         Logger.info("Installing Arenadata Tools packages")
         self.install_packages(env)
         #if any other install steps were needed they can be added here
+        Execute ('ln -sf /usr/lib/jvm/jre/lib/amd64/server/libjvm.so /usr/lib64/libjvm.so')
 
     def configure(self,env):
         import params
